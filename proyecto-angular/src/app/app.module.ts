8abe39e5ffa4310ab7949926ab00d9da8b33de67
Manuel Vargas <elkin.vargas02@uptc.edu.co>
2022-09-30 18:35:34 -0500

    feat:agregacion de formulario

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoguinModule } from './modules/loguin/loguin.module';
-
+import { FormularioCompraModule} from './modules/formulario-compra/formulario-compra.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { LoguinModule } from './modules/loguin/loguin.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoguinModule
+    LoguinModule,
+    FormularioCompraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
